91d1acadd9cb3f9b1186bad27136eaf12bf6e048
Tommi Siivola <tommi.siivola@eficode.com>
2013-11-21 08:36:34 +0200

    Improved application server start & stop

--- robot/tests/test_application.py
+++ robot/tests/test_application.py
@@ -1,15 +1,41 @@
 
-from subprocess import Popen
+from subprocess import Popen, PIPE
 from time import sleep
+import threading
+import re
 
-server_process = None
+class PipeReader(threading.Thread):
+  def __init__(self, pipe):
+    threading.Thread.__init__(self)
+    self.read_buffer = ""
+    self.pipe = pipe
+    self.continue_reading = True
+
+  def run(self):
+    for line in self.pipe:
+      if not self.continue_reading: break
+      self.read_buffer += line
+
+
+server_proc = None
 
 def start_server():
-  global server_process
-  server_process = Popen(["java", "-jar", "server/ROOT.war"])
-  sleep(5.0)
+  global server_proc
+  server_proc = Popen(["java", "-jar", "server/ROOT.war"], stdout=PIPE)
+  reader = PipeReader(server_proc.stdout)
+  reader.start()
+  server_ready = False
+  for i in range(1, 30):
+    sleep(0.5)
+    if re.search("^.*Winstone Servlet Engine.* running.*$",reader.read_buffer):
+      server_ready = True
+      break
+  reader.continue_reading = False
 
 def stop_server():
-  global server_process
-  server_process.terminate()
+  global server_pid
+  try:
+    server_proc._process.destroy()
+  except IOError:
+    pass
 
